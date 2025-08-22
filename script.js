d8dbe5c0a143b4c16b75c34a94ca0329865e402b
Josmoo <52058577+josmoo@users.noreply.github.com>
2025-08-22 11:11:35 -0500

    reworked rps to have ui https://www.theodinproject.com/lessons/foundations-revisiting-rock-paper-scissors

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const ROUND_NUMBER = 5;
 
 function getComputerChoice() { //the illusive 3-sided die
     let rand = Math.random();
@@ -16,53 +17,99 @@ function getHumanChoice() {
 }
 
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for(let i = 0; i < 5; i++){
-        playRound(getHumanChoice(),getComputerChoice());
-    }
-    console.log("Your score: " + humanScore + ", and computer score: " + computerScore);
-
-    
-    function playRound(humanChoice, computerChoice) {
-        console.log("The computer chose " + computerChoice);
-        if (humanChoice == computerChoice) {
-            console.log("Draw!")
-        }
-        if (humanChoice == "rock") {
-            if (computerChoice == "scissors") {
-                console.log("You won!");
-                humanScore++;
-                return;
-            }
-            console.log("The computer won...");
-            computerScore++;
-            return;
-        }
-        if (humanChoice == "paper") {
-            if (computerChoice == "rock") {
-                console.log("You won!");
-                humanScore++;
-                return;
-            }
-            console.log("The computer won...");
-            computerScore++;
-            return;
-        }
-        if (humanChoice == "scissors") {
-            if (computerChoice == "paper") {
-                console.log("You won!");
-                humanScore++;
-                return;
-            }
-            console.log("The computer won...");
-            computerScore++;
-            return;
-        }
-        console.log("big error uh oh oopsy");
-        return;
-    }
 }
 
-playGame();
+function playRound(humanChoice) {
+    const computerChoice = getComputerChoice();
+    let results = "The computer chose " + computerChoice + ". ";
+
+    if (humanChoice == computerChoice) {
+        return results + "It's a draw!";
+    }
+    if (humanChoice == "rock") {
+        if (computerChoice == "scissors") {
+            incrementHumanScore();
+            return results + "You won!";
+        }
+        incrementComputerScore();
+        return results + "The computer won...";
+    }
+    if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            incrementHumanScore();
+            return results + "You won!";
+        }
+        incrementComputerScore();
+        return results + "The computer won...";
+    }
+    if (humanChoice == "scissors") {
+        if (computerChoice == "paper") {
+            incrementHumanScore();
+            return results + "You won!";
+        }
+        incrementComputerScore();
+        return results + "The computer won...";
+    }
+    console.log("big error uh oh oopsy look at results.textContent = playRound");
+    return;
+}
+
+function incrementHumanScore(){
+    const humanScore = document.querySelector("#humanScore");
+    humanScore.textContent = parseInt(humanScore.textContent) + 1;
+}
+
+function incrementComputerScore(){
+    const computerScore = document.querySelector("#computerScore");
+    computerScore.textContent = parseInt(computerScore.textContent) + 1;
+}
+
+
+const buttonsMenu = document.querySelector(".button-container");
+const resultsDisplay = document.querySelector(".results-display");
+
+buttonsMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const results = document.createElement("p");
+
+    switch (target.id) {
+        case 'rockBtn':
+            results.textContent = playRound("rock");
+            break;
+
+        case 'paperBtn':
+            results.textContent = playRound("paper");
+            break;
+
+        case 'scissorsBtn':
+            results.textContent = playRound("scissors");
+            break;
+
+        default:
+            console.log("big oopsy woopsy uh oh look at event listener");
+    }
+    resultsDisplay.appendChild(results);
+    checkWinner();
+});
+
+function checkWinner(){
+    const computerScore = document.querySelector("#computerScore");
+    if (computerScore.textContent == ROUND_NUMBER){
+
+        const winMessage = document.createElement("p");
+        winMessage.textContent = "In a best of " + ROUND_NUMBER + " match, you let the clanker win... :/";
+        resultsDisplay.appendChild(winMessage);
+        return;
+    }
+
+    const humanScore = document.querySelector("#humanScore");
+    if (humanScore.textContent == ROUND_NUMBER){
+        console.log("AOWJEFAISJDFOIASDF");
+        const winMessage = document.createElement("p");
+        winMessage.textContent = "In a best of " + ROUND_NUMBER + " match, you won!";
+        resultsDisplay.appendChild(winMessage);
+    }
+    //todo ideally should make player unable to continue playing
+}
+
+
